196faf4dfbaa5a389d5da8a9e4797abc6daa8989
Sunki Pang <pang@Sunkiui-MacBook-Pro.local>
2019-10-09 19:48:47 +0900

    debugginggit add .

--- NumberCross/NumberCross.c
+++ NumberCross/NumberCross.c
@@ -20,9 +20,8 @@ int main (int argc, char **argv)
         buffer = fgetc(fpc);
         if(feof(fpc)) break;
         if(buffer == ' ') {
+            col_buffer++;
             flag = 0;
-            if(flag == 1)
-                col_buffer++;
             //continue;
         }
         else if(buffer == '\n'){
